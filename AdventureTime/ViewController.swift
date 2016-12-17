9b314328a61448253c23321d0a89697646532277
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-12-17 11:51:53 -0500

    Super fun lab

--- AdventureTime/ViewController.swift
+++ AdventureTime/ViewController.swift
@@ -10,16 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var occupationLabel: UILabel!
+    @IBOutlet weak var speciesLabel: UILabel!
+    @IBOutlet weak var characterImageView: UIImageView!
+    
+    var finn: Character!
+    var jake: Character!
+    var bubblegum: Character!
+    var bmo: Character!
+    var lemongrab: Character!
+    var lsp: Character!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        createAllCharacters()
+    }
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        switch sender.titleLabel!.text! {
+        case "Finn":
+            updateViews(with: finn)
+        case "Jake":
+            updateViews(with: jake)
+        case "Bubblegum":
+            updateViews(with: bubblegum)
+        case "BMO":
+            updateViews(with: bmo)
+        case "Lemongrab":
+            updateViews(with: lemongrab)
+        case "LSP":
+            updateViews(with: lsp)
+        default: break
+        }
         
     }
-
-  
     
+    func createAllCharacters() {
+        finn = Character(name: "Finn The Human", species: "Human", occupation: "Hero", powerLevel: 50.0)
+        jake = Character(name: "Jake The Dog", species: "Dog", occupation: "Sidekick", powerLevel: 30.0)
+        bubblegum = Character(name: "Princess Bubblegum", species: "Human", occupation: "Candy", powerLevel: 30.0)
+        bmo = Character(name: "BMO", species: "What", occupation: "Living", powerLevel: 20.0)
+        lemongrab = Character(name: "Lemongrab", species: "Lemon Life", occupation: "Out Chea", powerLevel: 15.0)
+        lsp = Character(name: "Lumpy Space Princess", species: "WTF", occupation: "Lumpy", powerLevel: 100.0)
+    }
     
-   
-
+    func updateViews(with character: Character) {
+        characterImageView.image = character.displayImage()
+        nameLabel.text = character.name
+        occupationLabel.text = character.occupation
+        speciesLabel.text = character.species
+    }
+    
 }
 
